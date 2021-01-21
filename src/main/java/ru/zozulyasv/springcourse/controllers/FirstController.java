5c4e37065c41e3921a3f688641bb96742c162bae
Sergey Zozulya <zozulia.sergey2011@yandex.ru>
2021-01-21 19:12:09 +0300

    Lesson 20

--- src/main/java/ru/zozulyasv/springcourse/controllers/FirstController.java
+++ src/main/java/ru/zozulyasv/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.zozulyasv.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,11 +11,45 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequestMapping("/first")
 public class FirstController {
-    @GetMapping("/hello")
-    public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+    @GetMapping("/calculator")
+    public String helloPage(@RequestParam(value = "a", required = false) int a,
+                            @RequestParam(value = "b", required = false) int b,
+                            @RequestParam(value = "action", required = false) String action,
+                            Model model) {
+        double answer;
+        String actionSymbol;
 
-        System.out.println("Hello, " + name + " " + surname);
+        switch (action) {
+            case "multiplication":
+                actionSymbol = "*";
+                answer = a * b;
+                break;
+            case "addition":
+                actionSymbol = "+";
+                answer = a + b;
+                break;
+            case "subtraction":
+                actionSymbol = "-";
+                answer = a - b;
+                break;
+            case "division":
+                actionSymbol = "/";
+                answer = a / (double)b;
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + action);
+        }
+
+        if (action != null) {
+            model.addAttribute("answer", "Requested: "
+                    + a + " "
+                    + actionSymbol + " "
+                    + b + " "
+                    + "Answer: " +
+                    answer);
+        }
+        else  model.addAttribute("answer", "error");
+        
         return "first/hello";
     }
     @GetMapping("/goodbye")
