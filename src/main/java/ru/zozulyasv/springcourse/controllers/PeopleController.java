37ba47d430353df25dab90c069ed66715abf6865
Sergey Zozulya <zozulia.sergey2011@yandex.ru>
2021-01-24 11:51:37 +0300

    Lesson 25 SQL

--- src/main/java/ru/zozulyasv/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/zozulyasv/springcourse/controllers/PeopleController.java
@@ -1,11 +1,13 @@
 package ru.zozulyasv.springcourse.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.zozulyasv.springcourse.dao.PersonDAO;
 import ru.zozulyasv.springcourse.models.Person;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -27,7 +29,7 @@ public class PeopleController {
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model){
         //Получим одного человека по его id из DAO и передадим на отображение в представление
-        model.addAttribute("person", personDAO.show(id));
+//        model.addAttribute("person", personDAO.show(id));
         return "people/show";
     }
 
@@ -37,26 +39,34 @@ public class PeopleController {
     }
 
     @PostMapping
-    public String create(@ModelAttribute("person") Person person){
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "people/new";
+
         personDAO.save(person);
         return "redirect:/people";
     }
 
     @GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") int id){
-        model.addAttribute("person", personDAO.show(id));
+ //       model.addAttribute("person", personDAO.show(id));
         return "people/edit";
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
-        personDAO.update(id, person);
+    public String update(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors())
+            return "people/edit";
+//        personDAO.update(id, person);
         return "redirect:/people";
     }
 
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") int id){
-        personDAO.delete(id);
+//        personDAO.delete(id);
         return "redirect:/people";
     }
 }
--- src/main/java/ru/zozulyasv/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/zozulyasv/springcourse/dao/PersonDAO.java
@@ -3,42 +3,86 @@ package ru.zozulyasv.springcourse.dao;
 import org.springframework.stereotype.Component;
 import ru.zozulyasv.springcourse.models.Person;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
-    private List<Person> people;
     private static int PEOPLE_COUNT;
 
-    {
-        people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Nikita"));
-        people.add(new Person(++PEOPLE_COUNT, "Boka"));
-        people.add(new Person(++PEOPLE_COUNT, "Joka"));
-        people.add(new Person(++PEOPLE_COUNT, "Nikolay"));
+    private static final String URL = "jdbc:postgresql://localhost:5432/my_db";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public List<Person> index(){
+        List<Person> people = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while (resultSet.next()) {
+                Person person = new Person();
+
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setEmail(resultSet.getString("email"));
+                person.setAge(resultSet.getInt("age"));
+
+                people.add(person);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return people;
     }
 
-    public Person show(int id) {
-        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
-    }
+//    public Person show(int id) {
+//        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+//    }
 
     public void save(Person person){
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "INSERT INTO person VALUES(" + 1 + ",'" + person.getName() + "'," +
+                    person.getAge() + ",'" + person.getEmail() + "')";
+            statement.executeUpdate(SQL);
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
-    public void update(int id, Person updatedPerson) {
-        Person personToBeUpdated = show(id);
-        personToBeUpdated.setName(updatedPerson.getName());
-    }
-
-    public void delete(int id) {
-        people.removeIf(p -> p.getId() == id);
-    }
+//    public void update(int id, Person updatedPerson) {
+//        Person personToBeUpdated = show(id);
+//
+//        personToBeUpdated.setName(updatedPerson.getName());
+//        personToBeUpdated.setAge(updatedPerson.getAge());
+//        personToBeUpdated.setEmail(updatedPerson.getEmail());
+//    }
+//
+//    public void delete(int id) {
+//        people.removeIf(p -> p.getId() == id);
+//    }
 }
